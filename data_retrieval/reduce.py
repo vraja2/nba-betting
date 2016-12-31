d772b55368e175f701cfbc257ad2a9bebc51a5ab
Vignesh Raja <vignesh@optimizely.com>
2016-12-31 13:44:37 -0800

    Fix indents

--- data_retrieval/reduce.py
+++ data_retrieval/reduce.py
@@ -7,9 +7,9 @@ outfile = "all_boxscores.json"
 files_names = glob.glob(os.path.join('data', "*.json"))
 all_data = {}
 for f in files_names:
-	with open(f) as infile:
-		print os.path.basename(f)
-		all_data[os.path.basename(f)] = json.load(infile)
+  with open(f) as infile:
+    print os.path.basename(f)
+    all_data[os.path.basename(f)] = json.load(infile)
 
 with open(outfile, 'w') as outfile:
-	json.dump(all_data, outfile)
+  json.dump(all_data, outfile)
